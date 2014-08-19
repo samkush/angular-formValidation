6054cb3e989fb9d6c0a2f6dc6b31d99f35b347c5
yoshida hiroyuki <A13026@PC-4863.local>
2014-08-19 23:17:34 +0900

    first commit

--- angularFormValidataion.js
+++ angularFormValidataion.js
@@ -2,26 +2,98 @@
 
 angular
 .module('formValidation', [])
+.directive('formValidationDirectiveNoSpace', function() {
+  var _link = function (scope, element, attrs) {
+    element.bind('change', function(event){
+      var val = event.target.value;
+      val = val.replace(/\s+/g, "");
+      this.value = val;
+    });
+  }
+  return {
+    restrict: 'A',
+    link: _link,
+  }
+})
+.directive('formValidationDirectiveOneByteNum', function() {
+  var _link = function (scope, element, attrs) {
+    element.bind('change', function(event){
+      var val = event.target.value;
+      val = val.replace(/[０-９]/g, function(s) {
+          return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+      });
+      this.value = val;
+    });
+  }
+  return {
+    restrict: 'A',
+    link: _link,
+  }
+})
+.directive('formValidationDirectiveOneByte', function() {
+  var _link = function (scope, element, attrs) {
+    element.bind('change', function(event){
+      var val = event.target.value;
+      val = val.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+          return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+      });
+      this.value = val;
+    });
+  }
+  return {
+    restrict: 'A',
+    link: _link,
+  }
+})
+.directive('formValidationDirectiveLimit', function() {
+  var _link = function (scope, element, attrs) {
+    element.bind('keyup', function(event){
+      var len;
+      if(scope.isNewLine){
+        len = event.target.value.length;
+      }else{
+        var val = event.target.value.replace(/[\n\r]/g,"");
+        len = val.length;
+      }
+      scope.$apply(function () {
+        scope.form.username.$error.minlength = !(len >= Number(attrs.ngMinlength));
+        scope.form.username.$error.maxlength = !(Number(attrs.ngMaxlength) >= len);
+      });
+    });
+  }
+  return {
+    restrict: 'A',
+    link: _link,
+  }
+})
 .directive('formValidationDirectiveFile', function() {
-  return function(scope,elm,attr){
-    elm.bind('change', function(event){
-      var file = (event.dataTransfer)? event.dataTransfer.files[0]:event.target.files[0];
-      
-      var image = new Image();
-      var fileReader = new FileReader();
+  var _link = function (scope, element, attrs) {
+    element.bind('change', function(event){
 
-      var imageSize = {};
+      var
+      file = (event.dataTransfer)? event.dataTransfer.files[0]:event.target.files[0],
+      image = new Image(),
+      fileReader = new FileReader();
+
+      scope.$apply(function () {
+          scope.isFileSize = (scope.fileMaxSize >= file.size && file.size >= scope.fileMimSize);
+          scope.isFileType = (scope.fileType.indexOf(file.type.toLowerCase()) != -1);
+      });
 
       fileReader.onloadend = function(){
         image.src = fileReader.result;
-        imageSize.width = image.width;
-        imageSize.height = image.height;
-        alert('fileSizeWidth:' + imageSize.width);
-        alert('fileSizeHeight:' + imageSize.height);
+
+        scope.$apply(function () {
+            scope.isImageWidth = (scope.maxWidth >= image.width && image.width >= scope.mimWidth);
+            scope.isImageHeight = (scope.maxHeight >= image.height && image.height >= scope.mimHeight);
+        });
+
       };
       fileReader.readAsDataURL(file);
-      alert('fileSize:' + file.size);
-      alert('fileType:' + file.type);
     });
   }
+  return {
+    restrict: 'A',
+    link: _link,
+  };
 });
--- examples/examples.js
+++ examples/examples.js
@@ -6,5 +6,38 @@
 angular
 .module('formValidationApp', ['formValidation'])
 .controller('mainController', function($scope) {
+  /*
+  file.html
+  */
+  //初期化
+  $scope.isFileSize = true;
+  $scope.isFileType = true;
+  $scope.isImageWidth = true;
+  $scope.isImageHeight = true;
+  //条件
+  //最大サイズ
+  $scope.fileMaxSize = 200000;
+  //最小サイズ
+  $scope.fileMimSize = 100000;
+  //拡張子
+  $scope.fileType = ['image/png','image/gif'];
+  //最大サイズ
+  $scope.maxWidth = 500;
+  //最小サイズ
+  $scope.mimWidth = 100;
+  //最大サイズ
+  $scope.maxHeight = 500;
+  //最小サイズ
+  $scope.mimHeight = 100;
+  /*
+  custom.html
+  */
+})
+.controller('main2ndController', function($scope) {
+  /*
+  custom.html
+  */
+  //改行を文字数に含める
+  $scope.isNewLine = false;
 });
 }(window, document));
